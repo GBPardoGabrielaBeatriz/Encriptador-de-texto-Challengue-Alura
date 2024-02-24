09850fbceca9b23d86265dd3089fb6baf49e2318
Gabriela <gabb.pardo@gmail.com>
2024-02-23 21:21:44 -0300

    Emprolijamos logica de desencriptar, y agregamos el boton de copiar con funcionalidad

--- app.js
+++ app.js
@@ -66,12 +66,48 @@ console.log(textoYaEncriptado)
        // document.getElementById('texto-usuario').value=textoEncriptado;// Aca hay que volver a acceder a la variable, ya que la misma que fue llamada anteriormente ya fue modificada con el encriptado
         mensaje.textContent="Mensaje encriptado exitosamente!"
         texto.textContent="";
+        imagenDeEspera.src=""; 
+    }
+}
+}
+
+function desencriptarTexto(){
+    let textoUsuario=document.getElementById('texto-usuario').value;
+    let imagenDeEspera=document.getElementById('muñeco');
+    let texto=document.getElementById('texto');
+    let textoYaEncriptado=document.getElementById('texto-encriptado');
+
+    if (textoUsuario.length === 0) {
+        mensaje.textContent = "Ningun mensaje fue encontrado!";
+        texto.textContent="Ingrese el mensaje secreto para encriptar o desencriptar"
+        alert('Che, ingresa alguna palabra');
+        imagenDeEspera.src="";
+    }else {
+    if (!validacionDeTexto(textoUsuario)) {
+        mensaje.textContent = "El mensaje contiene caracteres inválidos!";
+    }else{
+        let textoDesencriptado=textoUsuario
+    
+.replaceAll("enter", "e")
+.replaceAll("imes", "im")
+.replaceAll("ai", "a")
+.replaceAll("ober", "o")
+.replaceAll("ufat", "u");
+//document.getElementById('texto-encriptado').value = textoEncriptado;
+textoYaEncriptado.innerText=textoYaEncriptado;
+console.log(textoYaEncriptado)
+       // document.getElementById('texto-usuario').value=textoEncriptado;// Aca hay que volver a acceder a la variable, ya que la misma que fue llamada anteriormente ya fue modificada con el encriptado
+        mensaje.textContent="Mensaje deseencriptado exitosamente!"
+        texto.textContent="";
         imagenDeEspera.src="";
     }
 }
 }
+
+/*
+//FALTA TERMINAR EL DESENCRIPTAR Y COPIAR
 function desencriptarTexto(){
-let textoUsuario=document.getElementById('texto-encriptado').value;
+let textoUsuario=document.getElementById('texto-usuario').value;
 let imagenDeEspera=document.getElementById('muñeco');
 let texto=document.getElementById('texto');
 let textoDesencriptado=textoUsuario
@@ -95,26 +131,44 @@ if(textoUsuario.length != 0){
 }  
 }
 
+**/
+
+function copiarTexto(){
+    let textoYaEncriptado=document.getElementById('texto-encriptado');
+
+    if (textoYaEncriptado.length === 0) {
+        mensaje.textContent = "No hay mensaje para copiar";
+       texto.textContent=""
+       // alert('Che, ingresa alguna palabra');
+        imagenDeEspera.src="";
+    }else {
+    textoYaEncriptado.select();
+    document.execCommand('copy');
+    mensaje.textContent="Mensaje copiado!"
+}}
 
 
 function validacionDeTexto(textoUsuario){
     let regex = /^[a-z]+$/;
     return regex.test(textoUsuario);
 }
- 
-
-
 
 //EVENTOS PARA EL BOTON 
 let encriptarBtn=document.getElementById('encriptar-btn');
 encriptarBtn.addEventListener("click", () => {
     encriptarTexto()
+
 }
 )
 
 let desencriptarBtn=document.getElementById('desencriptar-btn');
 desencriptarBtn.addEventListener("click",() =>{
-    desencriptarTexto
+    desencriptarTexto();
 })
+
+let copiarBtn=document.getElementById('copiar-btn');
+copiarBtn.addEventListener("click",() =>{
+    copiarTexto();
+} )
 
 
